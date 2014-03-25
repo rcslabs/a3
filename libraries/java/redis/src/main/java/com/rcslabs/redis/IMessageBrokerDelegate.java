528b6b9f2d6c5381fd0b3db1c80fec084a7bc3e4
yury.krikun <yury.krikun@44e08e39-4b91-0410-a4d4-833ecb1b66d7>
2014-03-25 17:09:30 +0000

    [alena3] remove redis library
    git-svn-id: https://svn.rcslabs.ru/subversion/rcslabs/SOURCES/trunk/alena3@6421 44e08e39-4b91-0410-a4d4-833ecb1b66d7

--- libraries/java/redis/src/main/java/com/rcslabs/redis/IMessageBrokerDelegate.java
+++
@@ -1,8 +0,0 @@
-package com.rcslabs.redis;
-
-public interface IMessageBrokerDelegate {
-	
-	void onMessageReceived(String channel, IMessage message);
-
-    void handleOnMessageException(IMessage message, Throwable e);
-}
