224867ffcf1824e0e135787aec1225ca6caecc24
yury.krikun <yury.krikun@44e08e39-4b91-0410-a4d4-833ecb1b66d7>
2014-09-25 09:00:28 +0000

    Added script for repack client dist (remove all proprietary rcslabs code for third-parties)
    git-svn-id: https://svn.rcslabs.ru/subversion/rcslabs/SOURCES/trunk/alena3@6570 44e08e39-4b91-0410-a4d4-833ecb1b66d7

---
+++ click2call/client/repack.sh
@@ -0,0 +1,8 @@
+tar xzf client-3.1.0-SNAPSHOT.tar.gz 
+rm www/a3/RTMP2JS.swf 
+rm www/a3/MEDIA2JS.swf 
+rm www/a3/a3.js
+rm www/a3/main.js 
+rm www/a3/libs.js 
+rm www/a3/editor.html 
+tar czf v2chat-dist.tar.gz www
