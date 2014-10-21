2f098b25c8351a1898434488058814054d02796b
yury.krikun <yury.krikun@44e08e39-4b91-0410-a4d4-833ecb1b66d7>
2014-10-21 11:11:06 +0000

    [alena3][a3client] Fix the DTMF event on Flash signaling.
    git-svn-id: https://svn.rcslabs.ru/subversion/rcslabs/SOURCES/trunk/alena3@6581 44e08e39-4b91-0410-a4d4-833ecb1b66d7

--- libraries/a3client/src/signaling.ts
+++ libraries/a3client/src/signaling.ts
@@ -355,7 +355,7 @@ module a3 {
 
 		dtmf(callId, dtmf) {
 			if(dtmf.length > 1){
-				throw new Exception("RTMP signaling unabled to send DTMF string, single char only");
+				throw new Error("RTMP signaling unabled to send DTMF string, single char only");
 			}
 			this.request("SEND_DTMF_SIGNAL", {
 				callId : callId,
